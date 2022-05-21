5f9025232bd5371e8a6b60f84c2332e2fb7bd551
MaksymNM <m.nikolaevich.m@gmail.com>
2022-05-21 19:25:38 +0300

    country app was added

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { useState } from 'react';
+
+import {Routes, Route} from 'react-router-dom';
+import {HomePage} from './pages/HomePage';
+import { Details } from './pages/Details';
+import { NotFound } from './pages/NotFound';
+
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+
+  return (
+    <>
+      <Header>
+
+      </Header>
+      <Main>
+        <Routes>
+          <Route exact path='/' element={<HomePage countries={countries} setCountries={setCountries} />} />
+          <Route  path='/country/:name' element={<Details/>} />
+          <Route path='*' element={<NotFound/>} />
+
+          
+        </Routes>
+      </Main>
+    </>
+  );
+}
+
+export default App;
